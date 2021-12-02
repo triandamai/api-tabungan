b46a4449e9de06740220579ee846e4aa578c08fc
trian damai <triannurizkillah@gmail.com>
2021-12-03 00:26:41 +0700

    feat: finishing membership feature

--- src/main/kotlin/app/trian/tabungan/controller/MemberController.kt
+++ src/main/kotlin/app/trian/tabungan/controller/MemberController.kt
@@ -17,91 +17,53 @@ class MemberController(
     private val memberService: MemberService
 ) {
     @GetMapping(
-        value = [
-            "api/v1/member/{id_user}"
-        ],
+        value = ["api/v1/member/{id_user}"],
         produces = ["application/json"]
     )
     fun getMemberByUser(@PathVariable("id_user") idUser:Long,pageable: Pageable) =
         memberService.getWalletByUser(idUser,pageable)
 
     @PostMapping(
-        value = [
-            "api/v1/member"
-                ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member"],
+        consumes = ["application/json", ],
+        produces = ["application/json"]
     )
     fun addMember(@RequestBody request: MemberRequest) =
         memberService.addNewMember(request)
 
     @PostMapping(
-        value = [
-            "api/v1/member/leave"
-        ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member/leave"],
+        consumes = ["application/json", ],
+        produces = ["application/json"]
     )
     fun leaveMember(@RequestBody request: MemberRequest)= memberService.leaveMembership(request)
 
     @PostMapping(
-        value = [
-            "api/v1/member/disable"
-        ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member/disable"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
     )
     fun disableMember(@RequestBody request: MemberRequest)= memberService.disableMember(request)
 
     @PostMapping(
-        value = [
-            "api/v1/member/makeowner"
-        ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member/makeowner"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
     )
     fun makeMemberOwner(@RequestBody request: MemberRequest)= memberService.makeMemberOwner(request)
 
     @PostMapping(
-        value = [
-            "api/v1/member/makeadmin"
-        ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member/makeadmin"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
     )
     fun makeMemberAdmin(@RequestBody request: MemberRequest)= memberService.makeMemberAdmin(request)
 
 
     @PostMapping(
-        value = [
-            "api/v1/member/makemember"
-        ],
-        consumes = [
-            "application/json",
-        ],
-        produces = [
-            "application/json"
-        ]
+        value = ["api/v1/member/makemember"],
+        consumes = ["application/json"],
+        produces = ["application/json"]
     )
     fun makeMemberAMember(@RequestBody request: MemberRequest)= memberService.makeMemberMember(request)
 
